fddcc79f05f60690bb9ac06e940f3df0e3d488c7
Ivan Bagaric <ivanbagaric@gmail.com>
2016-01-29 15:50:05 +0100

    separeta style on few sass files

--- functions.php
+++ functions.php
@@ -1,11 +1,7 @@
 <?php
-/**
- * Juliana engine room
- *
- * @package juliana
- */
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
-/**
- * Initialize all the things.
- */
-require get_stylesheet_directory() . '/inc/init.php';
+}
+?>
